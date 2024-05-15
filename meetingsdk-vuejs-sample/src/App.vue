f33e9b643b769d89630aab53ae3842f945107de9
Bach Le <lexuanbach952001@gmail.com>
2024-05-15 17:35:50 +0700

    first commit

---
+++ meetingsdk-vuejs-sample/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorldNew from "./components/HelloWorldNew.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <main>
+    <hello-world-new></hello-world-new>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ meetingsdk-vuejs-sample/src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { ZoomMtg } from "@zoom/meetingsdk";
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+
+var authEndpoint = "http://localhost:4000";
+var sdkKey = "yKIpTqWgRNGHMgDfyenu7A";
+var meetingNumber = "74138288338";
+var passWord = "";
+var role = 0;
+var userName = "Vue.js";
+var userEmail = "";
+var registrantToken = "";
+var zakToken = "";
+var leaveUrl = "http://localhost:5173/";
+
+function getSignature() {
+  fetch(authEndpoint, {
+    method: "POST",
+    body: JSON.stringify({
+      meetingNumber: meetingNumber,
+      role: role,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      startMeeting(data.signature);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function startMeeting(signature) {
+  document.getElementById("zmmtg-root").style.display = "block";
+
+  ZoomMtg.init({
+    leaveUrl: leaveUrl,
+    patchJsMedia: true,
+    success: (success) => {
+      console.log(success);
+      ZoomMtg.join({
+        signature: signature,
+        sdkKey: sdkKey,
+        meetingNumber: meetingNumber,
+        passWord: passWord,
+        userName: userName,
+        userEmail: userEmail,
+        tk: registrantToken,
+        zak: zakToken,
+        success: (success) => {
+          console.log(success);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    },
+    error: (error) => {
+      console.log(error);
+    },
+  });
+}
+</script>
+
+<template>
+  <h1>Zoom Meeting SDK Vue.js Sample</h1>
+
+  <button @click="getSignature">Join Meeting</button>
+</template>
+
+<style scoped></style>
